7468978b6b901265011b92acc73e5c8babfdfc83
Timo Glastra <timo@animo.id>
2023-11-27 13:52:46 +0700

    combine frame types
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/types/disclosure.ts
+++ src/types/disclosure.ts
@@ -1,17 +1,5 @@
+import { BaseFrame } from './frame'
+
 export type DisclosureItem = [string, string, unknown] | [string, unknown]
 
-export type DisclosureFrame<T> = T extends Array<unknown>
-    ? {
-          [K in keyof T]?: T[K] extends Record<string | number, unknown>
-              ? DisclosureFrame<T[K]> | boolean
-              : boolean
-      }
-    : T extends Record<string, unknown>
-    ? {
-          [K in keyof T]?: T[K] extends Array<unknown>
-              ? DisclosureFrame<T[K]> | boolean
-              : T[K] extends Record<string, unknown>
-              ? ({ __decoyCount?: number } & DisclosureFrame<T[K]>) | boolean
-              : boolean
-      } & { __decoyCount?: number } & Record<string, unknown>
-    : boolean
+export type DisclosureFrame<T> = BaseFrame<T, { __decoyCount?: number }>
--- src/types/present.ts
+++ src/types/present.ts
@@ -1,15 +1,3 @@
-export type PresentationFrame<T> = T extends Array<unknown>
-    ? {
-          [K in keyof T]?: T[K] extends Record<string | number, unknown>
-              ? PresentationFrame<T[K]> | boolean
-              : boolean
-      }
-    : T extends Record<string, unknown>
-    ? {
-          [K in keyof T]?: T[K] extends Array<unknown>
-              ? PresentationFrame<T[K]> | boolean
-              : T[K] extends Record<string, unknown>
-              ? PresentationFrame<T[K]> | boolean
-              : boolean
-      } & Record<string, unknown>
-    : boolean
+import { BaseFrame } from './frame'
+
+export type PresentationFrame<T> = BaseFrame<T, {}>
